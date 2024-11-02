0b6191ea17d97b31df95ea1a1a9ff106a7332660
kkatalz <zlata.kiev2004@gmail.com>
2024-11-02 20:09:10 +0200

    Added file lexer for lexical analysis

---
+++ lexer.py
@@ -0,0 +1,40 @@
+keywords = ["Позначити_точку", "Побудувати_відрізок",
+            "Побудувати_перпендикуляр", "до"]
+
+
+def lexer_analyzer(code):
+    tokens = []
+    current_token = ""
+
+    for char in code:
+        if char.isalpha() or char == '_':
+            current_token += char
+        elif char.isspace():
+            if current_token:
+                tokens.append(get_token_type(current_token))
+                current_token = ""
+        elif char in ";#":
+            if current_token:
+                tokens.append(get_token_type(current_token))
+                current_token = ""
+            tokens.append(("DELIMITER", char))
+        else:
+            if current_token:
+                tokens.append(get_token_type(current_token))
+                current_token = ""
+            print(f"Error: Invalid character '{char}'")
+
+    if current_token:
+        tokens.append(get_token_type(current_token))
+
+    return tokens
+
+
+def get_token_type(token):
+    if token in keywords:
+        return ("KEYWORD", token)
+    elif token.isidentifier():
+        return ("IDENTIFIER", token)
+    else:
+        print(f"Error: Invalid identifier: {token}")
+        return ("ERROR", token)
